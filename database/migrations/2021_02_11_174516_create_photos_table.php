b34ef5a1aa4bde7b3b028436b07ddf896022b741
Alexander-Zar <78043913+Alexander-Zar@users.noreply.github.com>
2021-03-06 01:15:34 +0200

    Update 2021_02_11_174516_create_photos_table.php

--- database/migrations/2021_02_11_174516_create_photos_table.php
+++ database/migrations/2021_02_11_174516_create_photos_table.php
@@ -9,24 +9,4 @@ class CreatePhotosTable extends Migration
     /**
      * Run the migrations.
      *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('photos', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 255)->nullable();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('photos');
-    }
-}
+ 
