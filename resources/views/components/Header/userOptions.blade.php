a6028ee0be3f7b87efe5b233ff562027efbe279c
tykatyk <tykatyk@gmail.com>
2021-03-05 11:58:58 +0200

    restored routes to shelters in userOptions.blade

--- resources/views/components/Header/userOptions.blade.php
+++ resources/views/components/Header/userOptions.blade.php
@@ -1,6 +1,6 @@
 <ul class="userOptions">
-	<li><a href="#">My Shelters</a></li>
-	<li><a href="#">Settings</a></li>
+	<li><a class="menuItems-a" href="/shelters/create">CreateShelter</a></li>
+	<li><a class="menuItems-a" href="/shelters/my">MyShelter</a></li>
 	@if(Auth::check())
 		<li class="logout">Log Out</li>
 	@else
