f8c52c1000f2fa70277f46fd3cfa1af88ca37687
Олександр <sasha.metelny.ok@gmail.com>
2021-03-06 14:44:47 +0200

    om4

--- resources/views/animals/create.blade.php
+++ resources/views/animals/create.blade.php
@@ -1,83 +1,100 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Animals</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/css/animal.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-  <div class='signup-container'>
-  <div class='left-container'>
-  <h1>
-    <i class='fas fa-paw'></i>
-    ANIMAL-SHELTER
-  </h1>
-  <div class='puppy'>
-    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/38816/image-from-rawpixel-id-542207-jpeg.png'>
-  </div>
-  </div>
-  <form action="/animals" method="post" enctype="multipart/form-data">
-  <div class='right-container'>
-  <header>
-    <div class='set'>
-      <div class='pets-name'>
-        <label for='name'>Name</label>
-        <input id='name' placeholder="Pet's name" type='text' name='name'>
-      </div>
-      <div class='pets-type'>
-        <label for='type'>Type</label>
-        <input id='type' placeholder="Pet's type" type='text' name='type'>
-      </div>
+<x-fullapp-layout>
+
+    <x-slot name="title">
+      Create
+    </x-slot>
+
+    <x-slot name="head">
+      <link rel="stylesheet" href="/css/animal.css">
+    </x-slot>
+
+
+    <div class="main-container">
+    <div class="body-container">
+
+    <div class='signup-container'>
+    <div class='left-container'>
+    <h1>
+      <i class='fas fa-paw'></i>
+      ANIMAL-SHELTER
+    </h1>
+    <div class='puppy'>
+      <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/38816/image-from-rawpixel-id-542207-jpeg.png'>
     </div>
-    <div class='set'>
-      <div class='pets-breed'>
-        <label for='breed'>Breed</label>
-        <input id='breed' placeholder="Pet's breed" type='text' name='breed'>
-      </div>
-      <div class='pets-birthday'>
-        <label for='age'>Age</label>
-        <input id='age' placeholder="Pet's age" type='text' name='age'>
-      </div>
     </div>
-    <div class='set'>
-      <div class='pets-gender'>
-        <label for='pet-gender-female'>Gender</label>
-        <div class='radio-container'>
-          <input checked='' id='pet-gender-female' name='sex' type='radio' value='0'>
-          <label for='pet-gender-female'>Female</label>
-          <input id='pet-gender-male' name='sex' type='radio' value='1'>
-          <label for='pet-gender-male'>Male</label>
+    <form action="/animals" method="post" enctype="multipart/form-data">
+    <div class='right-container'>
+    <header>
+      <div class='set'>
+        <div class='pets-name'>
+          <label for='name'>Кличка</label>
+          <input id='name' placeholder="Кличка тваринки" type='text' name='name'>
+        </div>
+        <div class='pets-type'>
+          <label for='type'>Тип</label>
+          <input id='type' placeholder="Тип тваринки" type='text' name='type'>
         </div>
       </div>
-      <div class='pets-spayed-neutered'>
-        <label for='pet-spayed'>Pet's status</label>
-        <div class='radio-container'>
-          <input checked='' id='pet-spayed' name='status' type='radio' value='0'>
-          <label for='pet-spayed'>Free</label>
-          <input id='pet-neutered' name='status' type='radio' value='1'>
-          <label for='pet-neutered'>Sheltered</label>
+      <div class='set'>
+        <div class='pets-breed'>
+          <label for='breed'>Порода</label>
+          <input id='breed' placeholder="Порода тваринки" type='text' name='breed'>
+        </div>
+        <div class='pets-birthday'>
+          <label for='age'>Вік</label>
+          <input id='age' placeholder="Вік тваринки" type='text' name='age'>
         </div>
       </div>
-    </div>
-    <br>
+      <div class='set'>
+        <div class='pets-gender'>
+          <label for='pet-gender-female'>Стать</label>
+          <div class='radio-container'>
+            <input checked='' id='pet-gender-female' name='sex' type='radio' value='0'>
+            <label for='pet-gender-female'>Дівчинка</label>
+            <input id='pet-gender-male' name='sex' type='radio' value='1'>
+            <label for='pet-gender-male'>Хлопчик</label>
+          </div>
+        </div>
+        <div class='pets-spayed-neutered'>
+          <label for='pet-spayed'>Статус</label>
+          <div class='radio-container'>
+            <input checked='' id='pet-spayed' name='status' type='radio' value='0'>
+            <label for='pet-spayed'>Вільний</label>
+            <input id='pet-neutered' name='status' type='radio' value='1'>
+            <label for='pet-neutered'>Прихищен.</label>
+          </div>
+        </div>
+      </div>
+      <br>
 
-    <div class='pets-photo'>
+      <div class='pets-photo'>
 
-      <label for="photo">Photo:</label>
-      <input type="file" id="photo" name="photo">
+        <label for="photo">Фото:</label>
+        <input type="file" id="photo" name="photo">
+      </div>
+    </header>
+    <footer>
+      <div class='set'>
+        <!--
+          <form action="/animals" method="get" >
+                @csrf
+                <input type="submit" id="back" value="Back">
+                @method('GET')
+              </form>
+            -->
+          <input type="submit" id='next' value="Добавити">
+
+
+      </div>
+    </footer>
     </div>
-  </header>
-  <footer>
-    <div class='set'>
-      <input type="submit" id='next' value="Add">
+    @csrf
+    </form>
     </div>
-  </footer>
   </div>
-  @csrf
-  </form>
-  </div>
-</body>
-</html>
+</div>
+
+
+      <x-scripts.img-preview img="logo" imgPreview="logo-preview" />
+
+</x-fullapp-layout>
--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -1,82 +1,92 @@
-<!DOCTPE html>
-<html>
-<head>
-<title>Animals</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-  <div class="input-group">
-    <form action="/animals/create" method="get">
-          @csrf
-          <input type="submit" class="btn-success" value="add animal">
-          @method('GET')
-        </form>
+<x-fullapp-layout>
+
+    <x-slot name="title">
+      Animal list
+    </x-slot>
+
+    <x-slot name="head">
+      <link rel="stylesheet" href="/css/animal.css">
+    </x-slot>
+
+    <div style="border: 2px solid black">
+    <table class="table table-striped">
+      <div style="float:right">
+        <div class="input-group">
+          <form action="/animals/create" method="get" >
+                @csrf
+                <input type="submit" class="btn-success" value=" Добавити ">
+                @method('GET')
+              </form>
+        </div>
+      </div>
+      <br>
+    <thead>
+    <tr>
+    <th>Id</th>
+    <th>Кличка</th>
+    <th>Порода</th>
+    <th>Вік</th>
+    <th>Тип</th>
+    <th>Фото</th>
+    <th>Стать</th>
+    <th>Статус</th>
+    <th>Керування</th>
+    </tr>
+    </thead>
+    @foreach ($animal as $animal)
+
+    <tr>
+    <td>{{ $animal->id }}</td>
+    <td>{{ $animal->name }}</td>
+    <td>{{ $animal->breed }}</td>
+    <td>{{ $animal->age }}</td>
+    <td>{{ $animal->type }}</td>
+    <td><img src="{{asset("storage/" .$animal->photo) }}"></td>
+
+    <td>
+    @if(is_null($animal->sex))
+    Не вказано
+    @elseif($animal->sex==1)
+    Хлопчик
+    @else
+    Дівчинка
+    @endif
+    </td>
+
+    <td>
+      @if(is_null($animal->status))
+      Не вказано
+      @elseif($animal->status==1)
+      Прихищений
+      @else
+      Вільний
+      @endif
+    </td>
+    <td>
+      <div class="input-group">
+      <form action="/animals/{{ $animal->id }}/edit" method="get">
+            @csrf
+            <input type="submit" class="btn-success" value=" Редагувати ">
+            @method('GET')
+          </form>
+      </div>
+      <br>
+      <div class="input-group">
+          <form action="/animals/{{ $animal->id }}" method="post">
+            @csrf
+            <input type="submit" class="btn-success" value=" Видалити ">
+            @method('DELETE')
+          </form>
+
+     </div>
+      </td>
+    </tr>
+    @endforeach
+
+    </table>
   </div>
-<table class="table table-striped">
-<thead>
-<tr>
-<th>Id</th>
-<th>Nick</th>
-<th>breed</th>
-<th>age</th>
-<th>type</th>
-<th>photo</th>
-<th>sex</th>
-<th>status</th>
-<th>options</th>
-</tr>
-</thead>
-@foreach ($animal as $animal)
 
-<tr>
-<td>{{ $animal->id }}</td>
-<td>{{ $animal->name }}</td>
-<td>{{ $animal->breed }}</td>
-<td>{{ $animal->age }}</td>
-<td>{{ $animal->type }}</td>
-<td><img src="{{asset("storage/" .$animal->photo) }}"></td>
 
-<td>
-@if(is_null($animal->sex))
-unknown
-@elseif($animal->sex==1)
-Male
-@else
-Female
-@endif
-</td>
+      <x-scripts.img-preview img="logo" imgPreview="logo-preview" />
 
-<td>
-  @if(is_null($animal->status))
-  unknown
-  @elseif($animal->status==1)
-  Sheltered
-  @else
-  Free
-  @endif
-</td>
-<td>
-  <div class="input-group">
-  <form action="/animals/{{ $animal->id }}/edit" method="get">
-        @csrf
-        <input type="submit" class="btn-success" value="edit animal">
-        @method('GET')
-      </form>
-
-      <form action="/animals/{{ $animal->id }}" method="post">
-        @csrf
-        <input type="submit" class="btn-success" value="remove animal">
-        @method('DELETE')
-      </form>
-
- </div>
-  </td>
-</tr>
-@endforeach
-
-</table>
-</body>
-</html>
+</x-fullapp-layout>
