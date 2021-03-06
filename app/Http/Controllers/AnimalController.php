4fecf5beb55279f0a5c7675df01d54082b7704a1
Alexander-Zar <78043913+Alexander-Zar@users.noreply.github.com>
2021-03-06 12:02:39 +0200

    Update AnimalController.php

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -59,9 +59,9 @@ class AnimalController extends Controller
       if ($request->file('photo')){
       $animals->photo = $request->file('photo')->store('avatars');
       }
-
       $animals->shelter_id = $request->input('shelter_id');
-        
+      $animals->save();
+      
       return redirect()->route('animals.index');
     }
 
