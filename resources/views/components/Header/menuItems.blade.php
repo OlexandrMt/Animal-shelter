a2453e4fa7280b7586d8fe44d134cad0974a1e2d
Natalia <makodaynata@gmail.com>
2021-03-20 14:28:45 +0200

    home team

--- resources/views/components/Header/menuItems.blade.php
+++ resources/views/components/Header/menuItems.blade.php
@@ -2,6 +2,8 @@
 	<ul class="menuItems">
 		<li class="menuCloseBtn">
 			<i class="bi bi-x-circle"></i>
+			<!-- <li><a href="/">Home</a> -->
+				<li><a href="/animals">Тварини</a>
 		<li><a href="/shelters">Притулки</a>
 			<li><a href="/team">Наша команда</a>
 	</ul>
