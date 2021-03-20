826c4b23d9da745f6b54fb353a7ea16a27df43a1
Natalia <makodaynata@gmail.com>
2021-03-20 15:15:33 +0200

    header

--- resources/views/components/Header/breadcrumb.blade.php
+++ resources/views/components/Header/breadcrumb.blade.php
@@ -25,6 +25,7 @@
 		<div class="container-fluid">
 				<ul class="breadcrumb">
 						<li class="breadcrumb-item"><a href="/">Головна</a></li>
+						<li class="breadcrumb-item"><a href="/animals">Тварини</a></li>
 						<li class="breadcrumb-item"><a href="/shelters">Притулки</a></li>
 						<li class="breadcrumb-item"><a href="/team">Наша команда</a></li>
 				</ul>
--- resources/views/components/Header/menuItems.blade.php
+++ resources/views/components/Header/menuItems.blade.php
@@ -1,10 +1,10 @@
 <div>
-	<!-- <ul class="menuItems">
+	<ul class="menuItems">
 		<li class="menuCloseBtn">
 			<i class="bi bi-x-circle"></i>
 			<!-- <li><a href="/">Home</a> -->
 				<li><a href="/animals">Тварини</a>
 		<li><a href="/shelters">Притулки</a>
 			<li><a href="/team">Наша команда</a>
-	</ul> -->
+	</ul>
 </div>
