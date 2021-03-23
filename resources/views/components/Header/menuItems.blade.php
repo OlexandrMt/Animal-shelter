884d484972fdf5f68301cef886f049716acfb518
Олександр <sasha.metelny.ok@gmail.com>
2021-03-23 11:33:30 +0200

    17

--- resources/views/components/Header/menuItems.blade.php
+++ resources/views/components/Header/menuItems.blade.php
@@ -1,14 +1,4 @@
 <div>
-<<<<<<< HEAD
-  <ul class="menuItems">
-    <li class="menuCloseBtn">
-      <i class="bi bi-x-circle"></i>
-      <!-- <li><a href="/">Home</a> -->
-        <li><a href="/animals">Тварини</a>
-    <li><a href="/shelters">Притулки</a>
-      <li><a href="/team">Наша команда</a>
-  </ul>
-=======
 	<ul class="menuItems">
 		<li class="menuCloseBtn">
 			<i class="bi bi-x-circle"></i>
@@ -17,5 +7,4 @@
 		<li><a href="/shelters">Притулки</a>
 			<li><a href="/team">Наша команда</a>
 	</ul>
->>>>>>> dev
 </div>
